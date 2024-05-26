5f912e203592689e50a0093329f9f120863ad5a6
DavidDevGt <josuedavidvl18@gmail.com>
2024-05-26 08:37:52 -0600

    cambiar config para correr todo con un comando docker

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: '',
+    password: 'rootpassword',
     database: 'goal_tracker_db'
 });
 
